45da6b2ef05387c5945b80ad2e42559df021dcac
aglili <cecilselorm34@gmail.com>
2024-12-27 18:03:45 +0000

    Update database backup command to use explicit host address

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def backup_database():
                 "docker", "exec", DB_CONTAINER_NAME, 
                 "pg_dump", 
                 "-U", DB_USER,
-                "-h", "localhost", 
+                "-h", "0.0.0.0", 
                 "-p", "5433", # Add explicit host
                 DB_NAME
             ],
